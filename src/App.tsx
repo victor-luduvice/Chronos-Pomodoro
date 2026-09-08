6e1f9699a5cf3635ac29d0bdbf7d025248ac0bcb
victor-luduvice <contatovct777@gmail.com>
2026-09-08 15:28:56 -0300

    configurando os ciclos

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu/App';
 import { CountDown } from './components/CountDown/App';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
 export function App() {
   return (
@@ -34,7 +35,7 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <p>Ciclos</p>
+               <Cycles />
             <p>0 0 0 0</p>
           </div>
 
