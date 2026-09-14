1779ef205b4c5ba9b5bda48c6d573f318523adcf
victor-luduvice <contatovct777@gmail.com>
2026-09-14 18:15:28 -0300

    editando os botoes

--- src/components/DefaultButton/index.tsx
+++ src/components/DefaultButton/index.tsx
@@ -3,12 +3,13 @@ import styles from './styles.module.css';
 
 type DefaultButtonProps = {
  icon: React.ReactNode;
+ color?: 'green' | 'red';
 } & React.ComponentProps<'button'>;
 
-export function DefaultButton({ icon, ...props}: DefaultButtonProps){
+export function DefaultButton({ icon, color = 'green',  ...props}: DefaultButtonProps){
   return(
       <>
-      <button className={styles.button}  {...props}>
+      <button className={`${styles.button} ${color === 'red' ? styles.red : styles.green}`}  {...props}>
         {icon}
       </button>
       </>
